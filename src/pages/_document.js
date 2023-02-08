d3c16d15247f8e5417e93924fda64fdb99f932b6
sha7hid <shahidshafadshahad@gmail.com>
2023-02-08 20:42:22 +0530

    deploy

---
+++ src/pages/_document.js
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+      <link href="https://fonts.googleapis.com/css2?family=Mochiy+Pop+One&display=swap" rel="stylesheet"></link>
+      <link href="https://fonts.googleapis.com/css2?family=Kalam:wght@300&display=swap" rel="stylesheet"></link>
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
---
+++ src/pages/published.js
@@ -0,0 +1,59 @@
+import Head from 'next/head'
+import styles from '@/styles/Home.module.css'
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Link from 'next/link';
+export const getStaticProps = async () => {
+    const res = await fetch('https://blogbackend-production-af81.up.railway.app/api/blogs');
+    const data = await res.json();
+    return{
+      props:{results:data}
+    }
+  }
+  
+export default function Published({results}) {
+    return (
+        <><Navbar bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home">
+                    <h2 className={styles.navBrand}>BlogIT</h2>
+                </Navbar.Brand>
+                <Link href='/' className={styles.navlink}>
+            Home
+          </Link>
+          <Link href='/published' className={styles.navlink}>
+            Published
+          </Link>
+            </Container>
+        </Navbar>
+        <style jsx>{`
+h1 {
+  font-family: 'Mochiy Pop One', sans-serif;
+}
+p {
+  font-family: 'Kalam', cursive;
+}
+`}</style>
+      {results.map(result =>(
+      <><div>
+              <h1 className={styles.heading}>{result.heading}</h1>
+              <p className={styles.date}>Published on {result.date}</p><br />
+              <div className={styles.image}>
+                  <img src={result.image} width='590' height='450' alt='' />
+              </div>
+              <p className={styles.content}>{result.content}</p>
+          </div>
+          <p className={styles.content}>❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️</p></>
+     ) 
+     )}
+      </>
+     
+   
+
+  
+  
+    );
+
+
+}
